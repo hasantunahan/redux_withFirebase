61c37fcdbb9c9c06c8c239a5e7a61005fd286cbd
hasantunahan <hasan.tunahan.ak@gazi.edu.tr>
2021-10-18 20:12:07 +0300

    ios login with google

--- src/core/init/navigation/navigation.js
+++ src/core/init/navigation/navigation.js
@@ -18,6 +18,8 @@ export const Navigation = props => {
       webClientId:
         '926170197189-2pb6s4qcthjv178b5esmniciltiisnb0.apps.googleusercontent.com',
       client_type: 3,
+      iosClientId :'926170197189-25d3sne7o3a65fk968c19c7pvn96kore.apps.googleusercontent.com',
+      offlineAccess: false
     });
     if (scheme !== 'dark') {
       dispatch(changeTheme(lightTheme));
